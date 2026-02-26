365e9aa655fd821a7d0cc067338e14829c05954e
Kactus <someaverageguy2@gmail.com>
2026-02-26 15:24:18 -0700

    ruhiel buff

--- GXCE/Codes/Source/Parts/PartData/CRBR/Body/BBRuhiel.asm
+++ GXCE/Codes/Source/Parts/PartData/CRBR/Body/BBRuhiel.asm
@@ -79,7 +79,7 @@ byte [2] |
 2, |    #Invulnerability
 0       #Invisibility
 half [16] |
-130, |  #Charge Damage
+140, |  #Charge Damage
 165, |  #Knockback Velocity
 80, |   #Knockback Angle
 20, |   #Homing
