ef46481ae2f8e32e8f28e55cb8f7e1894bf714e0
Kactus <someaverageguy2@gmail.com>
2026-03-12 18:54:39 -0600

    alexander slight nerf

--- GXCE/Codes/Source/Parts/PartData/CRBR/Body/WSRayLegend.asm
+++ GXCE/Codes/Source/Parts/PartData/CRBR/Body/WSRayLegend.asm
@@ -86,13 +86,13 @@ half [16] |
 10, |   #Homing
 10, |   #Startup Phase Duration (+3 for # of Frames)
 30, |   #Attack Phase Duration (+1 for # of Frames)
-0, |   #Recovery Phase Duration
+5, |   #Recovery Phase Duration
 250, |   #Startup Phase Speed
 125, |  #Attack Phase Speed
 0, |   #Recovery Phase Speed
 90, |    #Startup Phase Angle
 0, |    #Attack Phase Angle
--90, |  #Recovery Phase Angle
+-5, |  #Recovery Phase Angle
 60, |   #Hitbox Size Related
 20, |   #Hitbox Size Related
 -40       #Hitbox Size Related
